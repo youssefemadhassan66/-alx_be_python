1fbaa186ceddeb7164d7b232a4c5901685e8ed05
youssef_emad <youssefmeadhassan66@gmail.com>
2025-09-13 17:12:12 +0300

    initial Commit

---
+++ basic_operations.py
@@ -0,0 +1,7 @@
+number1 = 10
+number2 = 5
+print("Addition of " + str(number1) + " and " + str(number2) + " is " + str(number1 + number2))
+
+print("Subtraction of " + str(number1) + " and " + str(number2) + " is " + str(number1 - number2))
+
+print("Multiplication of " + str(number1) + " and " + str(number2) + " is " + str(number1 * number2))
