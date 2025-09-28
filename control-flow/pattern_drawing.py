09a45c7c848ce78d9ce9b26d961384cb8837cda2
Youssef Emad <youssefmeadhassan66@gmail.com>
2025-09-28 18:28:25 +0300

    updated pattern_drawing

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,7 +1,9 @@
 # Pattern Drawing with While and For Loops
 size = int(input("Enter the size of the pattern: "))
 
-for row in range(size):
+row = 0
+while row < size:
     for col in range(size):
         print("*", end="")
-    print()
+    print()  
+    row += 1
